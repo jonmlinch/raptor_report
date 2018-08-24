89af50c604ddf0b1bdc541dd3a765dc23102247c
Jonathan Linch <junglecat@Jonathans-MacBook-Pro.local>
2018-08-24 09:50:37 -0700

    Removed lingering references to admin

--- controllers/auth.js
+++ controllers/auth.js
@@ -27,7 +27,6 @@ router.get('/signup', function(req, res){
 
 router.post('/signup', function(req, res){
 	console.log(req.body)
-	req.body.admin = false;
 	db.user.findOrCreate({
 		where: { email: req.body.email },
 		defaults: req.body

0cffa685be38ea3abd98c7589247e19953eb4d9d
Jonathan Linch <junglecat@Jonathans-MacBook-Pro.local>
2018-08-24 08:17:07 -0700

    added .env file and session secret

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+//require .env file's variable
+require('dotenv').config();
+
 //add your requires modules
 var bodyParser = require('body-parser');
 var ejsLayouts = require('express-ejs-layouts');
@@ -14,7 +17,7 @@ app.set('view engine', 'ejs');
 app.use(ejsLayouts);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(session({
-	secret: 'abc',
+	secret: process.env.SESSION_SECRET,
 	resave: false,
 	saveUninitialized: true
 }));
